62da3c30e03179e64e8ba7939d62882cbef448fb
svk1427 <75059407+svk1427@users.noreply.github.com>
2021-01-23 16:57:44 +0530

    jenkins update

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "This is vamsi krishna" );
+        System.out.println( "changes for jenkins purpose" );
     }
 }
