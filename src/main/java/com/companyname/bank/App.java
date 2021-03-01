a6ada9ec703448429dca50b7456cc35fe81e5e3e
svk1427 <75059407+svk1427@users.noreply.github.com>
2021-03-01 16:57:28 +0530

    update file

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "changes for jenkins purpose" );
+        System.out.println( "changes for jenkins purpose new modifications" );
     }
 }
